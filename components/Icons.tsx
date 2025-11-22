5bcdb6a0f7a89b9f46976cf7a87324d952942349
Badrish PC <badrishms@gmail.com>
2025-11-22 14:50:01 +0100

    initial portfolio

---
+++ components/Icons.tsx
@@ -0,0 +1,35 @@
+
+import React from 'react';
+import { Github, Linkedin, Twitter, Mail, ExternalLink, MessageSquare, X, Send, ChevronRight, ArrowUpRight, Instagram, Check, Loader2, Calendar, Moon, Sun, Cloud, CloudRain, CloudSnow, Clock, Copy, Sparkles, Factory, Activity, Package, LineChart, Mouse, ArrowDown, MapPin } from 'lucide-react';
+
+export const Icons = {
+  Github,
+  Linkedin,
+  Twitter,
+  Mail,
+  ExternalLink,
+  MessageSquare,
+  X,
+  Send,
+  ChevronRight,
+  ArrowUpRight,
+  Instagram,
+  Check,
+  Loader2,
+  Calendar,
+  Moon,
+  Sun,
+  Cloud,
+  CloudRain,
+  CloudSnow,
+  Clock,
+  Copy,
+  Sparkles,
+  Factory,
+  Activity,
+  Package,
+  LineChart,
+  Mouse,
+  ArrowDown,
+  MapPin
+};
